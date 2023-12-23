ff74d2e6fa039d734136bb7af026a01ca85e5745
Jairo Canova <152497680+JairoCanova@users.noreply.github.com>
2023-12-23 06:51:06 -0300

    Year of birth within pi.py

--- year within pi.py
+++ year within pi.py
@@ -3,21 +3,27 @@
 from mpmath import mp
 
 def find_birth_year_in_pi(birth_year):
+    
     mp.dps = 10000
+    
     pi_digits = str(mp.pi)[2:]
+    
     birth_year_str = str(birth_year)
+    
     position = pi_digits.find(birth_year_str)
-    return position
+    
+    return position, pi_digits[:position+len(birth_year_str)]
 
 if __name__ == "__main__":
-    birth_year = input("Enter your birth year (YYYY): ")
+    birth_year = input("Enter your birth year (yyyy): ")
     
     try:
         birth_year = int(birth_year)
-        position = find_birth_year_in_pi(birth_year)
+        position, pi_until_birth_year = find_birth_year_in_pi(birth_year)
         
         if position != -1:
             print(f"The sequence {birth_year} appears in the decimal expansion of pi at position {position}.")
+            print(f"Decimal expansion of pi until your birth year: {pi_until_birth_year}")
         else:
             print(f"The sequence {birth_year} does not appear in the decimal expansion of pi.")
     except ValueError:
